d5dc024bd6fedd085d92a66ce6f38536ebb7c777
seijibas <seiji.basaglia@gmail.com>
2016-05-22 00:56:32 -0300

    Comitando.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+var myName = "Google";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+drawName(myName, letterColors);
+
+bubbleShape = 'circle';
+
+bounceBubbles();
